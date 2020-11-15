1abfbf0977010f7cb27e6fad69586b7d0db6cb73
Fedor Filimonov <f.filimonov@gmail.com>
2020-11-15 16:09:14 +0300

    Initial Commit

---
+++ Task Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task Manager
+//
+//  Created by Федор Филимонов on 15.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
