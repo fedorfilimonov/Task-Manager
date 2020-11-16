78044a2c1890ed652a597a48a7345e7f51c2fdcd
Fedor Filimonov <f.filimonov@gmail.com>
2020-11-16 20:01:22 +0300

    Completed lesson 5
    1. Задание на паттерн Composite.
    
    Создать приложение со списком задач, где для каждой задачи можно создать подзадачи, у них — еще подзадачи, и так до бесконечности.
    UI приложения: первый экран состоит из списка корневых задач. В navigation bar справа должна быть кнопка + (добавить задачу). Все задачи отображаются в обычной table view. В ячейке укажите название задачи и количество подзадач в ней. По нажатию на ячейку переходим на новый экран, представляющий собой экран этой задачи. На нем отображается список подзадач уже этой задачи. Здесь также есть плюсик в navigation bar для добавления подзадач и все они отображаются в table view. Таким образом, все экраны задач абсолютно одинаковы.
    Реализуйте древовидную иерархию задач, применив паттерн Composite.

---
+++ Task Manager/Screens/Main Screen/MainTaskTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MainTaskTableViewCell.swift
+//  Task Manager
+//
+//  Created by Федор Филимонов on 15.11.2020.
+//
+
+import UIKit
+
+class MainTaskTableViewCell: UITableViewCell {
+    
+    // UI
+    
+    @IBOutlet weak var task: UILabel!
+    
+    @IBOutlet weak var subtasksNumber: UILabel!
+
+}
---
+++ Task Manager/Screens/Subtasks/SubTaskTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SubTaskTableViewCell.swift
+//  Task Manager
+//
+//  Created by Федор Филимонов on 16.11.2020.
+//
+
+import UIKit
+
+class SubTaskTableViewCell: UITableViewCell {
+    
+    // UI
+    @IBOutlet weak var task: UILabel!
+    
+    @IBOutlet weak var subtasksNumber: UILabel!
+}
+
